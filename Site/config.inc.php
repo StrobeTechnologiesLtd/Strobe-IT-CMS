8cd4456386a978cc58c57ae952318053e9cc04aa
Robin Toy <robin@strobe-it.co.uk>
2015-03-22 09:48:27 +0000

    Base Setup & Config Files
    Uploaded / updated base setup SQL files for the database along with a
    base configuration file for the system

--- Site/config.inc.php
+++ Site/config.inc.php
@@ -5,19 +5,16 @@
  * Author: Robin Toy <robin@strobe-it.co.uk>
  * Company: Strobe Technologies Ltd T/a Strobe IT
  * File: config.inc.php
- * Version: 1.0
+ * Version: 1.1
  */
 // This page config additional bits like DB settings and is stored so not accessible from the browser
 	
-	$db_username = "hippotek_thorne";  //devcoadm
-	$db_password = "FpqJa8yd1";  //m@dwalk3r
-	$db_database = "hippotek_thornerev1"; //devco   //Database name
-	$db_host = "213.171.200.48";  //213.171.193.145
-	$db_type = "mysql";
-	//https://mysql.prositehosting.co.uk/
+	$db_username = "<USERNAME>";											// Database username
+	$db_password = "<PASSWORD>";											// Database password
+	$db_database = "<DB NAME>";												// Database name
+	$db_host = "<DB ADDRESS / IP>";											// DNS Address or IP Address of database server
+	$db_type = "mysql";														// Type of database server
 	
-	//define("SITE_ROOT", "http://www.thorneandcarter.com/");
-	//define("LIVE_PATH", "/home/linweb24/t/thorneandcarter.co.uk/user/htdocs/");
-	$livepath = "/home/linweb24/t/thorneandcarter.co.uk/user/htdocs";
-	$user_minpasslen = 5
+	$livepath = "/home/linweb24/t/thorneandcarter.co.uk/user/htdocs";		// Physical path / Scripting path to htdocs root
+	$user_minpasslen = 5													// Minimum length of passwords
 ?>
